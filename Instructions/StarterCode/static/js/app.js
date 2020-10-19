3cbfa8ae06f0af0753a7a47a0e627a357c06cd74
Kent Thomas <kent.thomas@Kents-MacBook-Air.local>
2020-10-19 14:51:53 -0400

    Reading in the data, but not working the way I would like.

--- Instructions/StarterCode/static/js/app.js
+++
@@ -1,4 +0,0 @@
-d3.json("samples.json").then((data)=>{
-    var otuSort = data.sort((a,b) => b.otu_ids - a.otu_ids);
-    console.log(otuSort)
-}) 
---
+++ Instructions/StarterCode/static/js/apptest.js
@@ -0,0 +1,22 @@
+var json_data = "../data/samples.json"
+// console.log(json_data)
+var bacteria_data = d3.json(json_data).then(function(data){
+    // console.log(data);
+
+    
+
+});
+
+// function plotData(id){
+//     d3.json("../data/samples.json").then((data)=>{
+//         console.log(data)
+//     }
+//     )};
+
+
+// var otu_id_sort = bacteria_data.sort((a, b) => b.samples-a.samples);
+// console.log(otu_id_sort);
+
+// var otu_id_sliced =  otu_id_sort.slice(0,10);
+// console.log(otu_id_sliced);
+
