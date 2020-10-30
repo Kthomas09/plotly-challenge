350533ecd9f7a59867e41084807af1b9c2df203d
Your Name <kent.thomas@Kents-MacBook-Air.local>
2020-10-30 14:24:03 -0400

    it is working but must work on building charts and the change of menu feature.

--- Instructions/StarterCode/static/js/app.js
+++ Instructions/StarterCode/static/js/app.js
@@ -1,19 +1,19 @@
 // function to build the necessary plots for the dashboard
-function onchange(data) {
+function buildData(sample) {
     // reading the sample.json into the JavaScript
     d3.json("samples.json").then((data) => {
-
+        
         // Setting the variables to read in the specific portions of the JSON
         var metadata = data.metadata;
         var names = data.names;
         var samples = data.samples;
-
+        
         // Setting the intial display message into drop down menu
-        console.log("We got here")
+    
         var message = ("Choose A Sample");
         d3.select("#selDataset")
-            .append("option")
-            .attr("value", message)
+        .append("option")
+        .attr("value", message)
             .html(message)
 
         // populating the dropdown menu with the sample names
@@ -23,18 +23,58 @@ function onchange(data) {
                 .attr("value", sample);
             row.text(sample);
         });
-        // function to change the drop down menu and append selected sample data.
-        function dropMenuChange(() => {
-            console.log(filterSamples)
-            console.log(filterMetadata)
-            var demographTable = d3.select("#demographic-table");
-            demographTable.html("");
-            var inputElement = d3.select("#selDataset");
-            var tableBody = d3.select("tbody");
-            var inputValue = d3.select("value");
+    });
+};
 
-            var filterMetadata = metadata.filter(d => d.id == inputValue);
-            var filterSamples = samples.filter(s => s.id == inputValue);
+// build chart function
+function buildCharts(metadata){
+    // slice data
+    // layout the chart
+    // put trace in 
+    // layout trace
+    // plot trace
+
+};
+
+// Initiate the entire function
+function init(){
+    var selector = d3.select("#selDataset");
+        d3.json("samples.json").then((data)=>{
+            var name = data.names;
+            name.forEach((sample)=>{
+                selector.append("option")
+                .property("value", sample)
+                .text(sample);
+            });
+        
+        var first = name[0]
+        buildData(first);
+        // call build chart function with first
+    })
+
+}
+
+
+// function menuChange(newsample)
+    // use the data to populate the build chart functions
+    // metadata changed, call function (below)
+    // buildData(newsample)
+    // buildChart(newsample)
+init();
+
+// 
+        // function to change the drop down menu and append selected sample data.
+        // function dropMenuChange(() => {
+        //     console.log(filterSamples)
+        //     console.log(filterMetadata)
+        //     var demographTable = d3.select("#demographic-table");
+        //     demographTable.html("");
+        //     var inputElement = d3.select("#selDataset");
+        //     var tableBody = d3.select("tbody");
+        //     var inputValue = d3.select("value");
+
+        //     var filterMetadata = metadata.filter(d => d.id == sample);
+        //     var filterSamples = samples.filter(s => s.id == sample);
 
 
 
@@ -69,14 +109,3 @@ function onchange(data) {
             //         };
             //         Plotly.newPlot("bar", barGraphData, barGraphLayout)
             //     })
-            // });
-        };
-    
-    
-
-    
-    )
-};
-    )};
-
-onchange(0)
