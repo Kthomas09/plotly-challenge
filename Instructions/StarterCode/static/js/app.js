70aa7ba99b91cc8a0371b0bc4eda3b0d60ab3061
Your Name <kent.thomas@Kents-MacBook-Air.local>
2020-11-14 12:03:19 -0500

    Cleaned code

--- Instructions/StarterCode/static/js/app.js
+++ Instructions/StarterCode/static/js/app.js
@@ -1,7 +1,8 @@
+// Function to format the demographic information into the drop down menu
 function gatheringDemographicInfo(id) {
     d3.json("samples.json").then((data) => {
         var metadata = data.metadata;
-        // console.log(metadata)
+        // Filter Metadata
         var info = metadata.filter(m => m.id.toString() == id)[0];
         var demoInfo = d3.select("#sample-metadata");
         demoInfo.html("");
@@ -11,24 +12,19 @@ function gatheringDemographicInfo(id) {
     });
 };
 
+// Function to format the plots
 function buildPlot(id) {
     d3.json("samples.json").then((data) => {
+        // Filter data and slice into the top ten samples.
         var one_sample_data = data.samples.filter(s => s.id.toString() == id)[0];
-        console.log(one_sample_data)
-        // ****
         var otu_ids = one_sample_data.otu_ids
         var sample_values = one_sample_data.sample_values.slice(0, 10).reverse();
         var otu_labels = one_sample_data.otu_labels.slice(0, 10).reverse();
-        // ****
-        // formatting data for plots
-        // var top_otu_samples = (one_sample_data.slice(0,10)).reverse();
-        // var top_otu_ids = top_otu_samples.map(d => "OTU " + d);
-        // var top_otu_labels = data.samples[0].otu_labels.slice(0,10).reverse();
+     
         // variable to plot the trace of the bar graph
         var trace_1 = {
             x: sample_values,
             y: otu_ids,
-            // text: top_otu_labels,
             marker: {
                 color: "red"
             },
@@ -37,6 +33,7 @@ function buildPlot(id) {
         };
         // variable to create the data
         var data_1 = [trace_1];
+
         // varable to plot the layout of the bar graph
         var layout_1 = {
             title: "Top Ten Operational Taxonomic Units",
@@ -67,31 +64,32 @@ function buildPlot(id) {
                 colorscale: "Earth"
             },
         };
+
         // variable to create the data
         var data_2 = [trace_2];
+
         // varable to plot the layout of the bar graph
         var layout_2 = {
             xaxis: { title: "OTU ID" },
             height: 600,
             width: 1000
         };
+
         // plot the bubble chart
         Plotly.newPlot("bubble", data_2, layout_2);
     })
 };
 
-
+// Function to build plot when changing drop down menu selection
 function changeDemographic(id) {
     buildPlot(id);
     gatheringDemographicInfo(id);
 };
 
+// Intializing the buld plot and gathering Demo info
 function init() {
-    console.log("Here")
     var dropDownMenu = d3.select("#selDataset");
-    console.log(dropDownMenu)
     d3.json("samples.json").then((data) => {
-        console.log(data)
         var sample_names = data.names
         sample_names.forEach((one_sample) => {
             dropDownMenu.append("option").text(one_sample).property("value", one_sample);
